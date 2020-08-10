65e3da4e5adea193bd35daadcc91ae72f60410ff
David <shogunuz@gmail.com>
2020-08-10 16:47:35 +0700

    v2.1.1 Added Cors

--- GenerationN/Startup.cs
+++ GenerationN/Startup.cs
@@ -26,6 +26,8 @@ namespace GenerationN
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
